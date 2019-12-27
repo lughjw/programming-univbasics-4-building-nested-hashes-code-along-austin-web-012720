bf81a6a3376a785cdaf5bfb1b681a7024580d9cd
Lugh Wilson <lughjw@gmail.com>
2019-12-27 02:38:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,16 +4,16 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         name: "Lord Montague",
-        age: 53
+        age: "53"
         },
       :matriarch => {
         #your key/value pairs here
         name: "Lady Montague",
-        age: 54
+        age: "54"
         },
       :hero => {
         name: "Romeo",
-        age: 15,
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
@@ -22,17 +22,17 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         name: "Lord Capulet",
-        age: 50
+        age: "50"
         },
       :matriarch => {
         #your key/value pairs here
         name: "Lady Capulet",
-        age: 51
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
         name: "Juliet",
-        age: 15,
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
